068196330db864a34332515bb4d87f77b7bccdbd
Stephan Zaat <s.zaat@amsterdam.nl>
2018-04-25 09:54:43 +0200

    Adding Tourbuzz config settings

--- Docker/docker-entrypoint.sh
+++ Docker/docker-entrypoint.sh
@@ -5,37 +5,66 @@ echo Starting server
 set -u
 set -e
 
-DB_HOST=${SYMFONY__TOURBUZZ__DATABASE_HOST:-tourbuzz-db.service.consul}
-DB_PORT=${SYMFONY__TOURBUZZ__DATABASE_PORT:-5432}
+DB_HOST=${TOURBUZZ__DATABASE_HOST:-tourbuzz-db.service.consul}
+DB_PORT=${TOURBUZZ__DATABASE_PORT:-5432}
+DB_NAME=${TOURBUZZ__DATABASE_NAME:-tourbuzz}
+DB_USER=${TOURBUZZ__DATABASE_USER:-tourbuzz}
+DB_PASSWORD=${TOURBUZZ__DATABASE_PASSWORD:-insecure}
+MESSAGEBIRD_API_KEY=${TOURBUZZ__MESSAGEBIRD_API_KEY:-placeholder}
+TRANSLATE_API_KEY=${TOURBUZZ__TRANSLATE_API_KEY:-placeholder}
+SENDGRID_API_KEY=${TOURBUZZ__SENDGRID_API_KEY:-placeholder}
 
-cat > /srv/web/tourbuzz-api/app/config/parameters.yml <<EOF
-parameters:
-   database_host: ${DB_HOST}
-   database_port: ${DB_PORT}
-   database_name: ${SYMFONY__TOURBUZZ__DATABASE_NAME}
-   database_user: ${SYMFONY__TOURBUZZ__DATABASE_USER}
-   database_password: ${SYMFONY__TOURBUZZ__DATABASE_PASSWORD}
-   mailer_transport: ${SYMFONY__TOURBUZZ__MAILER_TRANSPORT}
-   mailer_host: ${SYMFONY__TOURBUZZ__MAILER_HOST}
-   mailer_user: ${SYMFONY__TOURBUZZ__MAILER_USER}
-   mailer_password: ${SYMFONY__TOURBUZZ__MAILER_PASSWORD}
-   mailer_port: ${SYMFONY__TOURBUZZ__MAILER_PORT}
-   mailer_encryption: ${SYMFONY__TOURBUZZ__MAILER_ENCRYPTION}
-   mail_from: ${SYMFONY__TOURBUZZ__MAIL_FROM}
-   mail_cc: ${SYMFONY__TOURBUZZ__MAIL_CC}
-   auto_login_from_email: ${SYMFONY__TOURBUZZ__AUTO_LOGIN_FROM_EMAIL}
-   retention_policy: ${SYMFONY__TOURBUZZ__RETENTION_POLICY}
-   secret: ${SYMFONY__TOURBUZZ__SECRET}
-   messagebird_accountkey: ${SYMFONY__TOURBUZZ__MESSAGEBIRD_API_KEY}
-   messagebird_enable: ${SYMFONY__TOURBUZZ__MESSAGEBIRD_ENABLE}
-   sms_originator: ${SYMFONY__TOURBUZZ__SMS_ORGINATOR}
-   piwik_site_id: ${SYMFONY__TOURBUZZ__PIWIK_SITE_ID}
-   sms_disable: false
-   trusted_proxies:
-        - 127.0.0.1
-        - 10.0.0.0/8
-        - 172.16.0.0/12
-        - 192.168.0.0/16
+cat > /srv/web/tourbuzz-api/src/settings.php <<EOF
+<?php
+return [
+    'settings' => [
+        'displayErrorDetails' => true, // set to false in production
+        'addContentLengthHeader' => true, // Allow the web server to send the content-length header
+
+        // Renderer settings
+        'renderer' => [
+            'template_path' => __DIR__ . '/../templates/',
+        ],
+
+        // Monolog settings
+        'logger' => [
+            'name' => 'slim-app',
+            'path' => __DIR__ . '/../logs/app.log',
+            'level' => \Monolog\Logger::DEBUG,
+        ],
+        'doctrine' => [
+            'meta' => [
+                'entity_path' => [
+                    'src/entity'
+                ],
+                'auto_generate_proxies' => true,
+                'proxy_dir' =>  __DIR__.'/../cache/proxies',
+                'cache' => null,
+            ],
+            'connection' => [
+                'driver'   => 'pdo_pgsql',
+                'port'     => '${DB_PORT}',
+                'host'     => '${DB_HOST}',
+                'dbname'   => '${DB_NAME}',
+                'user'     => '${DB_USER}',
+                'password' => '${DB_PASSWORD}',
+            ]
+        ],
+        'haltesUrl'              => 'http://open.data.amsterdam.nl/ivv/touringcar/in_uitstaphaltes.json',
+        'messagesUrl'            => 'http://tourapi.b.nl/berichten/' . date("Y/m/d"),
+        'parkeerUrl'             => 'http://open.data.amsterdam.nl/ivv/touringcar/parkeerplaatsen.json',
+        'wachtwoordVergetenUrl'  => 'http://tour.b.nl/wachtwoordvergeten/',
+        'mailConfirmUrl'         => 'http://tour.b.nl/mailbevestigen/',
+        'mailUnsubscribeUrl'     => 'http://tour.b.nl/mailannuleren/',
+        'imageStoreRootPath'     => 'images/',
+        'imageStoreExternalPath' => 'http://tourapi.b.nl/images/',
+        'imageResizeUrl'         => 'http://tourapi.b.nl/afbeeldingen/',
+        'translateApiKey'        => '${TRANSLATE_API_KEY}',
+        'fromMail'               => 'noreply@tourbuzz.nl',
+        'sendgridApiKey'         => '${SENDGRID_API_KEY}',
+        'messagebirdApiKey'      => '${MESSAGEBIRD_API_KEY}'
+    ]
+];
 EOF
 
 #php tourbuzz-api/bin/console assetic:dump --env=prod
